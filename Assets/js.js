7a71f1d9bc7db85d0e21555d054f42836336db7c
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-03-12 16:49:15 -0500

    Adding OnClick & Local Storage

--- Assets/js.js
+++ Assets/js.js
@@ -17,8 +17,9 @@
 var timeblocks = function () {
     var htmlCode = ""
     for (var i = 9; i < 18; i++ ) {
+        var getLocalStorage = localStorage.getItem(i) || "";
         htmlCode += `<div class="row"><div class="time-block col-md-2">${i}</div>
-        <textarea class="col-md-8" id="${i}" rows="2" cols="70"></textarea>
+        <textarea class="col-md-8" id="${i}" rows="2" cols="70">${getLocalStorage}</textarea>
         <button class="saveBtn col-md-2">Save</button></div>`
     }
     $("#container").html(htmlCode);
@@ -31,4 +32,11 @@ $("#container").on("click", ".saveBtn", function(){
     var content = $(this).siblings("textarea").val();
     var timeBlock = $(this).siblings("textarea").attr("id");
     console.log(content, timeBlock);
+    localStorage.setItem(timeBlock, content);
 })
+
+// var user="Alex Wiles"
+// var greeting = "Welcome "+user+"!"
+// console.log("Welcome ",user,"!")
+// console.log(greeting)
+// console.log(`Welcome ${user} !`)
